81c7850a1c0d8e4fab32768835de84e97bc908d6
SV-user <127668595+Artemenko53@users.noreply.github.com>
2023-10-21 15:16:01 +0300

    Google search verification

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ if (localStorage.getItem("savedText")) {
 editableText.addEventListener("input", function () {
   localStorage.setItem("savedText", this.textContent);
 });
+
+const randomWords = fetch(
+  "https://random-word-api.vercel.app/api?words=2&length=6&type=capitalized"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    editableText.textContent = data[0] + " " + data[1];
+  });
+
+// console.log("Hey! ", randomWords);
